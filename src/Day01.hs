cab9d5adb343514bbd715dabfe7389580f53b642
darichey <darichey1@gmail.com>
2019-12-02 16:10:16 -0600

    Add explanations for solutions so far

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,11 +1,16 @@
+-- https://adventofcode.com/2019/day/1
 module Day01 (solutions) where
 
+-- |Reads the list of modules masses as a [Int]
 getInput :: IO [Int]
 getInput = (fmap read . lines) <$> readFile "input/day1.txt"
 
+-- |Claculates the simple amount of fuel required for a module
 fuel :: Int -> Int
 fuel mass = max ((mass `div` 3) - 2) 0
 
+-- |Calculates the total amount of fuel required for a module, taking into
+-- account the fact that fuel itself requires more fuel
 allFuel :: Int -> Int
 allFuel 0 = 0
 allFuel x = (y + allFuel y)
--- src/Day02.hs
+++ src/Day02.hs
@@ -1,16 +1,21 @@
-module Day02 (solutions) where
+-- https://adventofcode.com/2019/day/2
+module Day02 (solutions, solve, getInput) where
 
 import Data.List.Split
 import Data.Vector
 
 type Program = Vector Int
 
+-- |Reads the input IntCode program as a Program
 getInput :: IO Program
 getInput = (fromList . (fmap read) . (splitOn ",")) <$> readFile "input/day2.txt"
 
+-- |Runs an IntCode program given a noun and a verb. The result is the value
+-- at position 0 after the program completes.
 solve :: Int -> Int -> Program -> Int
 solve noun verb prog = Data.Vector.head (solve' 0 (fix prog))
     where
+        -- |Runs an IntCode program starting at the given pointer index
         solve' :: Int -> Program -> Program
         solve' ptr prog =
             case op of
@@ -23,18 +28,24 @@ solve noun verb prog = Data.Vector.head (solve' 0 (fix prog))
                 y  = prog ! (prog ! (ptr + 2))
                 z  = prog ! (ptr + 3)
 
+        -- |Replaces the values at positions 1 and 2 with the noun and verb
         fix :: Program -> Program
         fix prog = prog // [(1, noun), (2, verb)]
 
+-- |Solves part one by running the IntCode program with noun=12 and verb=2
 day02a :: Program -> Int
 day02a = solve 12 2
 
+-- |Solves part two by running the program first with noun=0,verb=0 and then
+-- with noun=1,verb=0. We realize that the result of the program has a linear
+-- relationship with the noun and verb. Specifically,
+-- res = base + noun*delta + base. We find the solution for the desired res
 day02b :: Program -> Int
 day02b prog = noun * 100 + verb
     where
-        res1 = solve 0 0 prog
-        res2 = solve 1 0 prog
-        (noun, verb) = divMod (19690720 - res1) (res2 - res1)
+        base  = solve 0 0 prog
+        delta = (solve 1 0 prog) - base
+        (noun, verb) = divMod (19690720 - base) delta
 
 -- Bruteforce Solution for part two
 -- day02b :: Program -> Int
