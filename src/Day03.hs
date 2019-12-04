3db739e4f27a8d93aacb648c54beabb9bf16e4ab
darichey <darichey1@gmail.com>
2019-12-03 20:12:45 -0600

    Clean up day 3 solution

--- src/Day03.hs
+++ src/Day03.hs
@@ -6,53 +6,38 @@ import Data.List
 import qualified Data.Set as S
 import qualified Data.Map as M
 
-data Direction = L | R | U | D deriving (Show)
-type Cmd = (Direction, Int)
-type Path = [Cmd]
+type Direction = Char
+type Path = [Direction]
 type Point = (Int, Int)
 
 getInput :: IO (Path, Path)
-getInput = (firstTwo . (fmap parsePath) . lines) <$> readFile "input/day3.txt"
+getInput = (firstTwo . fmap parsePath . lines) <$> readFile "input/day3.txt"
     where
         firstTwo (x:y:_) = (x,y)
-        parsePath = parseDirections . (splitOn ",")
-        parseDirections = fmap parseDirection
-        parseDirection (d:is) = case d of
-            'L' -> (L, i)
-            'R' -> (R, i)
-            'U' -> (U, i)
-            'D' -> (D, i)
-            where i = read is
+        parsePath = (parseDirection =<<) . splitOn ","
+        parseDirection (d:is) = replicate (read is) d
 
 points :: Path -> M.Map Point Int
-points = points' 0 (0,0)
-
-points' :: Int -> Point -> Path -> M.Map Point Int
-points' steps start [] = M.singleton start steps
-points' steps (x,y) ((dir,dist):path) = M.union (M.fromList $ between) (points' (steps+dist) ((fst . head) between) path)
+points path = M.fromListWith min (tail $ zip (scanl go (0,0) path) [0..])
     where
-        f :: Direction -> Int -> (Point, Int)
-        f dir dist' = case dir of
-            L -> ((x-dist', y), steps+dist')
-            R -> ((x+dist', y), steps+dist')
-            U -> ((x, y+dist'), steps+dist')
-            D -> ((x, y-dist'), steps+dist')
-        
-        between :: [(Point, Int)]
-        between = (f dir) <$> [dist,dist-1 .. 1]
+        go :: Point -> Direction -> Point
+        go (x,y) dir = case dir of
+            'L' -> (x - 1, y)
+            'R' -> (x + 1, y)
+            'U' -> (x, y + 1)
+            'D' -> (x, y - 1)
 
 distanceZero :: Point -> Int
 distanceZero (x1, y1) = (abs x1) + (abs y1)
 
-pathIntersections :: Path -> Path -> M.Map Point Int
-pathIntersections path1 path2 = M.intersectionWith (+) (points path1) (points path2)
+intersections :: Path -> Path -> M.Map Point Int
+intersections path1 path2 = M.intersectionWith (+) (points path1) (points path2)
 
 day03a :: Path -> Path -> Int
-day03a path1 path2 = distanceZero closestPoint
-    where closestPoint = minimumBy (comparing distanceZero) (M.keysSet $ pathIntersections path1 path2)
+day03a path1 path2 = minimum $ S.map distanceZero (M.keysSet $ intersections path1 path2)
 
 day03b :: Path -> Path -> Int
-day03b path1 path2 = minimum $ M.elems (pathIntersections path1 path2)
+day03b path1 path2 = minimum $ M.elems (intersections path1 path2)
 
 solutions :: IO (Int, Int)
 solutions = do
