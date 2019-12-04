bc7372a8387098b6e5deb9d31dcdc8ddcfb88a9b
darichey <darichey1@gmail.com>
2019-12-03 20:11:23 -0600

    Improve day 3 solution using Set and Map

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,76 +1,58 @@
 module Day03 where
 
 import Data.List.Split
-import Data.List
 import Data.Ord
-import Debug.Trace
-import Data.Set
+import Data.List
+import qualified Data.Set as S
+import qualified Data.Map as M
 
-data Direction = L Int | R Int | U Int | D Int deriving (Show)
-type Path = [Direction]
+data Direction = L | R | U | D deriving (Show)
+type Cmd = (Direction, Int)
+type Path = [Cmd]
 type Point = (Int, Int)
 
 getInput :: IO (Path, Path)
-getInput = do
-    fileContent <- readFile "input/day3.txt"
-    let fileLines = lines fileContent
-    let mapped = fmap ((fmap parseDirection) . splitOn ",") fileLines
-    return (mapped !! 0, mapped !! 1)
-
+getInput = (firstTwo . (fmap parsePath) . lines) <$> readFile "input/day3.txt"
     where
-        parseDirection :: String -> Direction
-        parseDirection (x:xs) = case x of
-            'L' -> L d
-            'R' -> R d
-            'U' -> U d
-            'D' -> D d
-            where d = read xs
+        firstTwo (x:y:_) = (x,y)
+        parsePath = parseDirections . (splitOn ",")
+        parseDirections = fmap parseDirection
+        parseDirection (d:is) = case d of
+            'L' -> (L, i)
+            'R' -> (R, i)
+            'U' -> (U, i)
+            'D' -> (D, i)
+            where i = read is
 
-locations :: Path -> Point -> Set Point
-locations [] p = singleton p
-locations (d:ds) p = Data.Set.union (fromList between) (locations ds (head between))
-    where between = locations' p d
+points :: Path -> M.Map Point Int
+points = points' 0 (0,0)
 
-locations' :: Point -> Direction -> [Point]
-locations' (x,y) (L d) = fmap (\d' -> ((x - d'), y)) [d,(d-1)..1]
-locations' (x,y) (R d) = fmap (\d' -> ((x + d'), y)) [d,(d-1)..1]
-locations' (x,y) (U d) = fmap (\d' -> (x, (y + d'))) [d,(d-1)..1]
-locations' (x,y) (D d) = fmap (\d' -> (x, (y - d'))) [d,(d-1)..1]
+points' :: Int -> Point -> Path -> M.Map Point Int
+points' steps start [] = M.singleton start steps
+points' steps (x,y) ((dir,dist):path) = M.union (M.fromList $ between) (points' (steps+dist) ((fst . head) between) path)
+    where
+        f :: Direction -> Int -> (Point, Int)
+        f dir dist' = case dir of
+            L -> ((x-dist', y), steps+dist')
+            R -> ((x+dist', y), steps+dist')
+            U -> ((x, y+dist'), steps+dist')
+            D -> ((x, y-dist'), steps+dist')
+        
+        between :: [(Point, Int)]
+        between = (f dir) <$> [dist,dist-1 .. 1]
 
-distance :: Point -> Point -> Int
-distance (x1, y1) (x2, y2) = (abs (x1-x2)) + (abs (y1-y2))
+distanceZero :: Point -> Int
+distanceZero (x1, y1) = (abs x1) + (abs y1)
 
-commonPoints :: Path -> Path -> Set Point
-commonPoints path1 path2 = intersection (locations path1 (0,0)) (locations path2 (0,0))
+pathIntersections :: Path -> Path -> M.Map Point Int
+pathIntersections path1 path2 = M.intersectionWith (+) (points path1) (points path2)
 
 day03a :: Path -> Path -> Int
-day03a path1 path2 = distance (0,0) (minimumBy (comparing (distance (0,0))) (commonPoints path1 path2))
-
-steps :: Point -> Path -> Int
-steps p path = steps' (0,0) p path
-
-steps' :: Point -> Point -> Path -> Int
-steps' p1 p2 _ | p1 == p2 = 0
-steps' p1 p2 (d:ds) = 1 + (steps' (go1 p1 d) p2 newPath)
-    where
-        newPath = case d of
-            (L 1) -> ds
-            (R 1) -> ds
-            (U 1) -> ds
-            (D 1) -> ds
-            (L x) -> (L (x-1)):ds
-            (R x) -> (R (x-1)):ds
-            (U x) -> (U (x-1)):ds
-            (D x) -> (D (x-1)):ds
-
-        go1 :: Point -> Direction -> Point
-        go1 (x,y) (L _) = (x-1,y)
-        go1 (x,y) (R _) = (x+1,y)
-        go1 (x,y) (U _) = (x,y+1)
-        go1 (x,y) (D _) = (x,y-1)
+day03a path1 path2 = distanceZero closestPoint
+    where closestPoint = minimumBy (comparing distanceZero) (M.keysSet $ pathIntersections path1 path2)
 
 day03b :: Path -> Path -> Int
-day03b path1 path2 = findMin . Data.Set.map (\p -> steps p path1 + steps p path2) $ commonPoints path1 path2
+day03b path1 path2 = minimum $ M.elems (pathIntersections path1 path2)
 
 solutions :: IO (Int, Int)
 solutions = do
