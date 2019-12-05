e75b19a7aa9e2b363d55307da8539e13fa63fe71
darichey <darichey1@gmail.com>
2019-12-05 01:37:02 -0600

    Small clean up to day 5

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,102 +1,114 @@
 module Day05 where
 
-import Data.Char
 import Data.List.Split
-import Debug.Trace
 import qualified Data.Vector as V
 
 type Program = V.Vector Int
 data Op = Add | Mul | In | Out | JumpT | JumpF | Less | Equals | Terminate deriving (Show)
-data PMode = Pos | Imm deriving (Show)
-data Ins = Ins Op PMode PMode PMode deriving (Show)
-
-leftPad :: Char -> String -> Int -> String
-leftPad with str num = (replicate (num - length str) with) ++ str
+data ParamMode = Pos | Imm deriving (Show)
+data Ins = Ins Op ParamMode ParamMode ParamMode deriving (Show)
 
 parseIns :: Int -> Ins
 parseIns ins = Ins (op de) (mode c) (mode b) (mode a)
     where
         (a:b:c:de) = leftPad '0' (show ins) 5
+
+        leftPad :: Char -> String -> Int -> String
+        leftPad with str num = (replicate (num - length str) with) ++ str
+
         op :: String -> Op
-        op "01" = Add
-        op "02" = Mul
-        op "03" = In
-        op "04" = Out
-        op "05" = JumpT
-        op "06" = JumpF
-        op "07" = Less
-        op "08" = Equals
-        op "99" = Terminate
-        mode :: Char -> PMode
-        mode '0' = Pos
-        mode '1' = Imm
+        op s = case s of
+            "01" -> Add
+            "02" -> Mul
+            "03" -> In
+            "04" -> Out
+            "05" -> JumpT
+            "06" -> JumpF
+            "07" -> Less
+            "08" -> Equals
+            "99" -> Terminate
+
+        mode :: Char -> ParamMode
+        mode c = case c of
+            '0' -> Pos
+            '1' -> Imm
 
 run :: [Int] -> Program -> [Int]
-run input prog = fst $ run' 0 input [] prog
+run input = fst . run' 0 input []
+    where
+        run' :: Int -> [Int] -> [Int] -> Program -> ([Int], Program)
+        run' (-1) _     output prog = (output, prog)
+        run' ptr  input output prog = run' ptr' input' (output' ++ output) prog'
+            where
+                ins = parseIns $ prog V.! ptr
+                (ptr', input', output', prog') = apply ptr input prog ins
 
-run' :: Int -> [Int] -> [Int] -> Program -> ([Int], Program)
-run' (-1) _ output prog = (output, prog)
-run' ptr input output prog = let
-    ins = traceShowId $ parseIns (prog V.! ptr)
-    (ptr', input', output', prog') = apply ptr input ins prog
-
-    in run' ptr' input' (output' ++ output) prog'
-
-apply :: Int -> [Int] -> Ins -> Program -> (Int, [Int], [Int], Program)
-apply ptr input (Ins Add mode1 mode2 mode3) prog = (ptr + 4, input, [], prog')
+apply :: Int -> [Int] -> Program -> Ins -> (Int, [Int], [Int], Program)
+apply ptr input prog (Ins Add mode1 mode2 _) = (ptr + 4, input, [], prog')
     where
         x = param (ptr + 1) mode1 prog
         y = param (ptr + 2) mode2 prog
         z = prog V.! (ptr + 3)
         prog' = prog V.// [(z, x + y)]
 
-apply ptr input (Ins Mul mode1 mode2 mode3) prog = (ptr + 4, input, [], prog')
+apply ptr input prog (Ins Mul mode1 mode2 _) = (ptr + 4, input, [], prog')
     where
         x = param (ptr + 1) mode1 prog
         y = param (ptr + 2) mode2 prog
         z = prog V.! (ptr + 3)
         prog' = prog V.// [(z, x * y)]
 
-apply ptr input (Ins In mode1 mode2 mode3) prog = (ptr + 2, tail input, [], prog')
+apply ptr input prog (Ins In mode1 _ _) = (ptr + 2, tail input, [], prog')
     where
         x = prog V.! (ptr + 1)
         prog' = prog V.// [(x, head input)]
 
-apply ptr input (Ins Out mode1 mode2 mode3) prog = (ptr + 2, input, [x], prog)
+apply ptr input prog (Ins Out mode1 _ _) = (ptr + 2, input, [x], prog)
     where
         x = param (ptr + 1) mode1 prog
 
-apply ptr input (Ins JumpT mode1 mode2 mode3) prog = (ptr', input, [], prog)
+apply ptr input prog (Ins JumpT mode1 mode2 _) = (ptr', input, [], prog)
     where
         x = param (ptr + 1) mode1 prog
         y = param (ptr + 2) mode2 prog
         ptr' = if x /= 0 then y else ptr + 3
 
-apply ptr input (Ins JumpF mode1 mode2 mode3) prog = (ptr', input, [], prog)
+apply ptr input prog (Ins JumpF mode1 mode2 _) = (ptr', input, [], prog)
     where
         x = param (ptr + 1) mode1 prog
         y = param (ptr + 2) mode2 prog
         ptr' = if x == 0 then y else ptr + 3
 
-apply ptr input (Ins Less mode1 mode2 mode3) prog = (ptr + 4, input, [], prog')
+apply ptr input prog (Ins Less mode1 mode2 _) = (ptr + 4, input, [], prog')
     where
         x = param (ptr + 1) mode1 prog
         y = param (ptr + 2) mode2 prog
         z = prog V.! (ptr + 3)
         prog' = prog V.// [(z, if x < y then 1 else 0)]
 
-apply ptr input (Ins Equals mode1 mode2 mode3) prog = (ptr + 4, input, [], prog')
+apply ptr input prog (Ins Equals mode1 mode2 _) = (ptr + 4, input, [], prog')
     where
         x = param (ptr + 1) mode1 prog
         y = param (ptr + 2) mode2 prog
         z = prog V.! (ptr + 3)
         prog' = prog V.// [(z, if x == y then 1 else 0)]
 
-apply ptr input (Ins Terminate _ _ _) prog = (-1, input, [], prog)
+apply ptr input prog (Ins Terminate _ _ _) = (-1, input, [], prog)
 
-param :: Int -> PMode -> Program -> Int
+param :: Int -> ParamMode -> Program -> Int
 param ptr Pos prog = prog V.! (prog V.! ptr)
 param ptr Imm prog = prog V.! ptr
 
 getInput :: IO Program
 getInput = (V.fromList . (fmap read) . (splitOn ",")) <$> readFile "input/day5.txt"
+
+day05a :: Program -> Int
+day05a  = head . run [1]
+
+day05b :: Program -> Int
+day05b = head . run [5]
+
+solutions :: IO (Int, Int)
+solutions = do
+    input <- getInput
+    return (day05a input, day05b input)
--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ import Day01
 import Day02
 import Day03
 import Day04
+import Day05
 
 main :: IO ()
 main = do
@@ -11,6 +12,7 @@ main = do
     printSolutions "Day 2" Day02.solutions
     printSolutions "Day 3" Day03.solutions
     printSolutions "Day 4" Day04.solutions
+    printSolutions "Day 5" Day05.solutions
 
 printSolutions :: String -> IO (Int, Int) -> IO ()
 printSolutions day solsM = do
