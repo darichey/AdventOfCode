5dc7e974e96a81adc7178475e202a1d80b69a01d
darichey <darichey1@gmail.com>
2019-12-05 23:03:10 -0600

    Further day 5 changes

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,22 +1,34 @@
 module Day05 where
 
 import Data.List.Split
+import Data.List
+import Data.Tuple
 import qualified Data.Vector as V
 
-data Program = Program (V.Vector Int) deriving Show
+data IntCode = IntCode (V.Vector Int) deriving Show
+data Program = Program Int [Int] [Int] IntCode deriving Show
 
 getInput :: IO Program
-getInput = (Program . V.fromList . (fmap read) . (splitOn ",")) <$> readFile "input/day5.txt"
+getInput = ((Program 0 [] []) . IntCode . V.fromList . (fmap read) . (splitOn ",")) <$> readFile "input/day5.txt"
 
-valueOf :: Param -> Program -> Int
-valueOf (Pos x) prog = valueAt x prog
-valueOf (Imm x) _    = x
+valueOf :: IntCode -> Param -> Int
+valueOf code (Pos x) = code ! x
+valueOf _    (Imm x) = x
 
-valueAt :: Int -> Program -> Int
-valueAt x (Program code) = code V.! x
+(!) :: IntCode -> Int -> Int
+(IntCode code) ! x = code V.! x
 
-update :: Param -> Int -> Program -> Program
-update (Pos z) xy (Program code) = Program $ code V.// [(z, xy)]
+values :: IntCode -> [Param] -> [Int]
+values = fmap . valueOf
+
+update :: Param -> Int -> IntCode -> IntCode
+update (Pos z) xy (IntCode code) = IntCode $ code V.// [(z, xy)]
+
+combine :: (Int -> Int -> Int) -> IntCode -> Param -> Param -> Int
+combine f _    (Imm x) (Imm y) = f x y
+combine f code (Imm x) y       = f x (valueOf code y)
+combine f code x      (Imm y)  = f (valueOf code x) y
+combine f code x       y       = f (valueOf code x) (valueOf code y)
 
 data Param = Pos Int
            | Imm Int 
@@ -33,92 +45,89 @@ data Ins = Add Param Param Param
          | Terminate
          deriving Show
 
-parseIns :: Int -> Program -> Ins
-parseIns ptr prog =
-    case de of
-        "01" -> Add    (param 1 c) (param 2 b) (param 3 a)
-        "02" -> Mul    (param 1 c) (param 2 b) (param 3 a)
-        "03" -> In     (param 1 c)
-        "04" -> Out    (param 1 c)
-        "05" -> JumpT  (param 1 c) (param 2 b)
-        "06" -> JumpF  (param 1 c) (param 2 b)
-        "07" -> Less   (param 1 c) (param 2 b) (param 3 a)
-        "08" -> Equals (param 1 c) (param 2 b) (param 3 a)
-        "99" -> Terminate
+nextIns :: Program -> Ins
+nextIns (Program ptr _ _ code) =
+    case op of
+        1 -> Add    (param 1 c) (param 2 b) (param 3 a)
+        2 -> Mul    (param 1 c) (param 2 b) (param 3 a)
+        3 -> In     (param 1 c)
+        4 -> Out    (param 1 c)
+        5 -> JumpT  (param 1 c) (param 2 b)
+        6 -> JumpF  (param 1 c) (param 2 b)
+        7 -> Less   (param 1 c) (param 2 b) (param 3 a)
+        8 -> Equals (param 1 c) (param 2 b) (param 3 a)
+        99 -> Terminate
     where
-        (a:b:c:de) = leftPad '0' (show (valueAt ptr prog)) 5
+        [e,d,c,b,a] = digits 5 (code ! ptr)
+        op = d * 10 + e
 
-        leftPad :: Char -> String -> Int -> String
-        leftPad with str num = (replicate (num - length str) with) ++ str
-
-        param :: Int -> Char -> Param
+        param :: Int -> Int -> Param
         param offset mode = case mode of
-            '0' -> Pos (valueAt (ptr + offset) prog)
-            '1' -> Imm (valueAt (ptr + offset) prog)
+            0 -> Pos 
+            1 -> Imm
+            $ (code ! (ptr + offset))
 
-run :: [Int] -> Program -> [Int]
-run input = fst . run' 0 input []
-    -- where
-run' :: Int -> [Int] -> [Int] -> Program -> ([Int], Program)
-run' (-1) _ output prog = (output, prog)
-run' ptr input output prog = run' ptr' input' output' prog'
-    where
-        f = apply (parseIns ptr prog)
-        (ptr', input', output', prog') = f ptr input output prog
+        digits :: Int -> Int -> [Int]
+        digits n num = take n (unfoldr (\b -> Just $ swap (divMod b 10)) num)
 
-apply :: Ins -> Int -> [Int] -> [Int] -> Program -> (Int, [Int], [Int], Program)
-apply ins ptr input output prog = case ins of
-    (Add p1 p2 p3) -> (ptr + 4, input, output, prog')
+withInput :: [Int] -> Program -> Program
+withInput input (Program ptr _ output code) = Program ptr input output code
+
+terminated :: Program -> Bool
+terminated (Program ptr _ _ _) = ptr == -1
+
+output :: Program -> [Int]
+output (Program _ _ output _) = output
+
+run :: Program -> [Int]
+run = output . until terminated apply
+
+apply :: Program -> Program
+apply ps@(Program ptr input output code) = case (nextIns ps) of
+    (Add p1 p2 p3) -> Program (ptr + 4) input output code'
         where
-            x = valueOf p1 prog
-            y = valueOf p2 prog
-            prog' = update p3 (x + y) prog
+            code' = update p3 (combine (+) code p1 p2) code
 
-    (Mul p1 p2 p3) -> (ptr + 4, input, output, prog')
+    (Mul p1 p2 p3) -> Program (ptr + 4) input output code'
         where
-            x = valueOf p1 prog
-            y = valueOf p2 prog
-            prog' = update p3 (x * y) prog
+            code' = update p3 (combine (*) code p1 p2) code
 
-    (In p1) -> (ptr + 2, tail input, output, prog')
+    (In p1) -> Program (ptr + 2) input' output code'
         where
-            prog' = update p1 (head input) prog
+            (i:input') = input
+            code' = update p1 i code
 
-    (Out p1) -> (ptr + 2, input, x:output, prog)
+    (Out p1) -> Program (ptr + 2) input (x:output) code
         where
-            x = valueOf p1 prog
+            x = valueOf code p1
 
-    (JumpT p1 p2) -> (ptr', input, output, prog)
+    (JumpT p1 p2) -> Program ptr' input output code
         where
-            x = valueOf p1 prog
-            y = valueOf p2 prog
+            [x, y] = values code [p1, p2]
             ptr' = if x /= 0 then y else ptr + 3
 
-    (JumpF p1 p2) -> (ptr', input, output, prog)
+    (JumpF p1 p2) -> Program ptr' input output code
         where
-            x = valueOf p1 prog
-            y = valueOf p2 prog
+            [x, y] = values code [p1, p2]
             ptr' = if x == 0 then y else ptr + 3
 
-    (Less p1 p2 p3) -> (ptr + 4, input, output, prog')
+    (Less p1 p2 p3) -> Program (ptr + 4) input output code'
         where
-            x = valueOf p1 prog
-            y = valueOf p2 prog
-            prog' = update p3 (if x < y then 1 else 0) prog
+            [x, y] = values code [p1, p2]
+            code' = update p3 (if x < y then 1 else 0) code
 
-    (Equals p1 p2 p3) -> (ptr + 4, input, output, prog')
+    (Equals p1 p2 p3) -> Program (ptr + 4) input output code'
         where
-            x = valueOf p1 prog
-            y = valueOf p2 prog
-            prog' = update p3 (if x == y then 1 else 0) prog
+            [x, y] = values code [p1, p2]
+            code' = update p3 (if x == y then 1 else 0) code
 
-    Terminate -> (-1, input, output, prog)
+    Terminate -> Program (-1) input output code
 
 day05a :: Program -> Int
-day05a  = head . run [1]
+day05a = head . run . withInput [1]
 
 day05b :: Program -> Int
-day05b = head . run [5]
+day05b = head . run . withInput [5]
 
 solutions :: IO (Int, Int)
 solutions = do
