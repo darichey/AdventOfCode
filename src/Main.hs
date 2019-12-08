df849c2594c6e4b8265f10f4783b8b222607c9f8
darichey <darichey1@gmail.com>
2019-12-08 02:38:06 -0600

    Support non-Int solutions

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module Main where
 
 import Day01
@@ -9,6 +12,15 @@ import Day06
 import Day07
 import Day08
 
+class Solution a where
+    showSol :: a -> String
+
+instance {-# OVERLAPPING #-} Solution String where
+    showSol = id
+
+instance (Show a) => Solution a where
+    showSol = show
+
 main :: IO ()
 main = do
     printSolutions "Day 1" Day01.solutions
@@ -18,10 +30,11 @@ main = do
     printSolutions "Day 5" Day05.solutions
     printSolutions "Day 6" Day06.solutions
     printSolutions "Day 7" Day07.solutions
+    printSolutions "Day 8" Day08.solutions
 
-printSolutions :: String -> IO (Int, Int) -> IO ()
+printSolutions :: (Solution a, Solution b) => String -> IO (a, b) -> IO ()
 printSolutions day solsM = do
     (part1, part2) <- solsM
-    putStrLn $ day
-    putStrLn $ "  " ++ (show part1)
-    putStrLn $ "  " ++ (show part2)
+    putStrLn $ " == " ++ day ++ " == "
+    putStrLn $ showSol part1
+    putStrLn $ showSol part2
